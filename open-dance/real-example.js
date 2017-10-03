a7dbc1161a496406d89e3f6778c581eea3b57cb4
Malte Ubl <malte.ubl@gmail.com>
2017-10-03 15:14:13 -0700

    Test

---
+++ open-dance/real-example.js
@@ -0,0 +1,35 @@
+// https://www.google.com/search?q=Barack+Obama+and+Malia+had+an+awesome+night+out+in+NYC+and+looked+awesome+doing+it
+
+(function() {
+  var links = document.querySelectorAll('.amp_r');
+  for (var i = 0; i < links.length; i++) {
+    augment(links[i]);
+  }
+  var iframe = document.createElement('iframe');
+  iframe.src = 'https://cramforce.github.io/playground/open-dance/example.html';
+  iframe.style.cssText = 'width: 412px; height: 732px; position:absolute; left: -1000px;';
+  iframe.name = 'prerenderFrame';
+  document.body.appendChild(iframe);
+
+  var prefetchCacheStub = document.createElement('link');
+  prefetchCacheStub.rel = 'prefetch';
+  prefetchCacheStub.href = 'https://cramforce.github.io/playground/open-dance/cache-stub2.html';
+  document.head.appendChild(prefetchCacheStub);
+
+  var prefetchPubStub = document.createElement('link');
+  prefetchPubStub.rel = 'prefetch';
+  prefetchPubStub.setAttribute('crossorigin', '');  // No cookies
+  prefetchPubStub.href = 'https://cramforce.github.io/playground/open-dance/pub-stub2.html';
+
+  document.head.appendChild(iframe);
+
+  function augment(a) {
+    a.addEventListener('click', onclick, true);
+  }
+
+  function onclick(e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    window.open('https://cramforce.github.io/playground/open-dance/pub-stub2.html', '_blank');
+  }
+})()
